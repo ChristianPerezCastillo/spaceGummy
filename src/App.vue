f61e6465a846359b69f43ca44f52613cab508028
ChristianPerezCastillo <ch.hristian96@gmail.com>
2022-12-04 18:43:27 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,357 @@
+
+<template>
+<div class="container text-center m-1">
+    <div class="row">
+
+
+        
+
+        <!--  -->
+        <div class="col-md-4 mt-4 p-3">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="./assets/img/gummy.png" class="card-img-top" alt="...">
+                </div>
+            </div>
+           
+            <div class="row" id="tamaño">
+                <div class="col-md-12">
+                    <div class="card text-center bg-transparent glass">
+                        <div class="card-header text-light">
+                            <h2>Elige el tamaño de tu pedido.</h2>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title text-light">¿Qué tamaño vas a querer?</h5>
+                            <div class="container">
+                                <div class="row justify-content-center m-4">
+                                    <div class="col-6 col-sm-4 col-md-6 p-4" id="tamañoChi">
+                        <div class="bg-transparent" style="width: 5rem;" id="">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="..." style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Chi</p>
+                                <span>$40</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-4 col-md-6 " id="tamañoGrd">
+                        <div class="bg-transparent" style="width: 8rem;" id="">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="" style="width: 8rem; height: 8rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Grd</p>
+                                <span>$65</span>
+                            </div>
+                        </div>
+                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--  -->
+        <div class="col-md-8">
+            <div class="container text-center glass">
+                
+                <div class="row m-4 justify-content-center" id="gomitas">
+                    <h2 class="text-light">GOMITAS</h2>
+                    <div class="col-6 col-sm-4 col-md-2 p-4" id="panditaSelect">
+                        <div class="bg-transparent" style="width: 5rem;" id="panditas">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="..." style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Panditas</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-4 col-md-2 p-4" id="manguitoSelect">
+                        <div class="bg-transparent" style="width: 5rem;" id="manguitos">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="..." style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Manguitos</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-4 col-md-2 p-4" id="gusanitoSelect">
+                        <div class="bg-transparent" style="width: 5rem;" id="gusanitos">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="..." style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Gusanitos</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-4 col-md-2 p-4" id="aciditoSelect">
+                        <div class="bg-transparent" style="width: 5rem;" id="aciditos">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="..." style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Aciditos</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-4 col-md-2 p-4" id="mangoEnchiladoSelect">
+                        <div class="bg-transparent" style="width: 5rem;" id="mangosEnchilados">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="..." style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Mangos Enchilados</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-4 col-md-2 p-4" id="picafresaSelect">
+                        <div class="bg-transparent" style="width: 5rem;" id="picafresas">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="..." style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Picafresa</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="row m-4 my-1 justify-content-center">
+                    <div class="col-6 col-sm-4 col-md-2 p-4" id="aritoSelect">
+                        <div class="bg-transparent" style="width: 5rem;" id="aritos">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="..." style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Aritos</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-4 col-md-2 p-4" id="">
+                        <div class="bg-transparent" style="width: 5rem;" id="">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="..." style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Dulcigomas</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-4 col-md-2 p-4" id="">
+                        <div class="bg-transparent" style="width: 5rem;" id="">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="..." style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Tira de mango enchilado</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-4 col-md-2 p-4" id="">
+                        <div class="bg-transparent" style="width: 5rem;" id="">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="..." style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Patas de gallo</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-4 col-md-2 p-4" id="">
+                        <div class="bg-transparent" style="width: 5rem;" id="">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="..." style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Gomitas de pinguino</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-4 col-md-2 p-4" id="">
+                        <div class="bg-transparent" style="width: 5rem;" id="">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="..." style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Aritos Enchilados</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="row p-2">
+                    
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-6">
+                        <h2 class="text-light">PAPITAS</h2>
+                        <div class="row">
+                            <div class="col-6 col-sm-6 col-md-6 p-4" id="chipsSelect">
+                        <div class="bg-transparent" style="width: 8rem;" id="chips">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="..." style="width: 8rem; height: 8rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Saladas</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-6 col-md-6 p-4">
+                        <div class="bg-transparent" style="width: 8rem;">
+                            <img src="./assets/img/gummy.png" class="card-img-top" style="width: 8rem; height: 8rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Adobadas</p>
+                            </div>
+                        </div>
+                    </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-6">
+                        <h2 class="text-light">CACAHUA</h2>
+                        <div class="row">
+                            
+                    <div class="col-6 col-sm-6 col-md-6 p-4">
+                        <div class="bg-transparent" style="width: 8rem;">
+                            <img src="./assets/img/gummy.png" class="card-img-top" style="width: 8rem; height: 8rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Hot nuts</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-6 col-md-6 p-4">
+                        <div class="bg-transparent" style="width: 8rem;" id="chips">
+                            <img src="./assets/img/gummy.png" class="card-img-top" style="width: 8rem; height: 8rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Cacahuates enchilados</p>
+                            </div>
+                        </div>
+                    </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="row m-4 justify-content-center">
+                    
+                    <div class="col-12 col-sm-12 col-md-6">
+                        <h2 class="text-light">FRUTAS</h2>
+                        <div class="row">
+                            <div class="col-6 col-sm-6 col-md-6 p-4" id="chipsSelect">
+                        <div class="bg-transparent" style="width: 7rem;" id="chips">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="..." style="width: 7rem; height: 7rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Pepino</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-6 col-md-6 p-4">
+                        <div class="bg-transparent" style="width: 7rem;">
+                            <img src="./assets/img/gummy.png" class="card-img-top" style="width: 7rem; height: 7rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Zanahoria</p>
+                            </div>
+                        </div>
+                    </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6 col-sm-6 col-md-6 p-4" id="chipsSelect">
+                        <div class="bg-transparent" style="width: 7rem;" id="chips">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="..." style="width: 7rem; height: 7rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Sandia</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-6 col-md-6 p-4">
+                        <div class="bg-transparent" style="width: 7rem;">
+                            <img src="./assets/img/gummy.png" class="card-img-top" style="width: 7rem; height: 7rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Piña</p>
+                            </div>
+                        </div>
+                    </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-6 col-sm-6 ">
+                        <h2 class="text-light">SALSAS</h2>
+                        <div class="row">
+                            <div class="col-4 col-sm-4 col-md-4" id="chipsSelect">
+                        <div class="bg-transparent" style="width: 5rem;" id="chips">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="..." style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Chamoy natural</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4 col-sm-4 col-md-4">
+                        <div class="bg-transparent" style="width: 5rem;">
+                            <img src="./assets/img/gummy.png" class="card-img-top" style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Botanera</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4 col-sm-4 col-md-4">
+                        <div class="bg-transparent" style="width: 5rem;" id="chips">
+                            <img src="./assets/img/gummy.png" class="card-img-top" style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Maggi</p>
+                            </div>
+                        </div>
+                    </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-4 col-sm-4 col-md-4" id="chipsSelect">
+                        <div class="bg-transparent" style="width: 5rem;" id="chips">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="..." style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Inglesa</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4 col-sm-4 col-md-4">
+                        <div class="bg-transparent" style="width: 5rem;">
+                            <img src="./assets/img/gummy.png" class="card-img-top" style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Jugo de limon</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4 col-sm-4 col-md-4">
+                        <div class="bg-transparent" style="width: 5rem;" id="chips">
+                            <img src="./assets/img/gummy.png" class="card-img-top" style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Miguelito</p>
+                            </div>
+                        </div>
+                    </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-4 col-sm-4 col-md-4" id="chipsSelect">
+                        <div class="bg-transparent" style="width: 5rem;" id="chips">
+                            <img src="./assets/img/gummy.png" class="card-img-top" alt="..." style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Tajin</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4 col-sm-4 col-md-4">
+                        <div class="bg-transparent" style="width: 5rem;">
+                            <img src="./assets/img/gummy.png" class="card-img-top" style="width: 5rem; height: 5rem;">
+                            <div class="card-body">
+                                <p class="card-text text-light">Salsa valentina</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                        </div>
+                    </div>
+                    
+                </div>
+
+                
+            </div>
+            
+        </div>
+        <!--  -->
+        <div class="col-md-12 col-lg-12 mt-4 p-3">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card text-center bg-transparent glass">
+                        <div class="card-header text-light">
+                            <h2>¿ESTAS LISTO?</h2>
+                        </div>
+                        <div class="card-body">
+                            <p>Presiona el boton para realizar tu pedido y envianos un WhatsApp para confirmar de recibido.</p>                            <br>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col" id="contenedorCanvas">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <button class="btn btn-primary" id="send">Go to WhatsApp</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</div>
+</template>
+
+
+
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import html2canvas from 'html2canvas'
+import './assets/main.css'
+
+createApp(App).mount('#app')
+
+
