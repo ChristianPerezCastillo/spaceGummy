673d8122acc149b35c6e1bedbcf705a5184cf7f0
ChristianPerezCastillo <ch.hristian96@gmail.com>
2022-12-09 20:24:32 -0600

    Modificacion de estilos

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="glassWhite container d-flex flex-column justify-content-center align-items-center text-center position-relative">
+  <div class="glass container d-flex flex-column justify-content-center align-items-center text-center position-relative">
              
-  <img src="../assets/img/gummy.png" class="img-fluid mt-4" alt="..." style="width: 20rem;">
+  <div class="col-md-6">
+    <img src="../assets/img/gummy.png" class="img-fluid mt-4" alt="..." style="width: 20rem;">
+  <p class="text-light">Un dulce pecado.</p>
   
+  </div>
 </div>
 </template>
 
--- src/components/Paquete.vue
+++ src/components/Paquete.vue
@@ -1,8 +1,8 @@
 <template>  
 <div class="p-4 glass container d-flex flex-column justify-content-center align-items-center text-center position-relative" style="width: 50rem;">
              <div class="row m-4">
-                <div class="col-md-6"><button class="button-67" role="button">P. chico <span>$75</span></button></div>
-                <div class="col-md-6"><button class="button-67" role="button">P.chico $60</button></div>
+                <div class="col-sm-12 col-md-6"><button class="button-67" role="button">P. chico <span>$75</span></button></div>
+                <div class="col-sm-12 col-md-6"><button class="button-67" role="button">P.chico $60</button></div>
              </div>
     
              
