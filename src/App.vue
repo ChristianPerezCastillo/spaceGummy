1859eebfe6394af88bc45d47d6f9af8370f4233d
ChristianPerezCastillo <ch.hristian96@gmail.com>
2022-12-10 10:08:55 -0600

    Modificacion de estilos

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,20 @@
                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 col-xxl-6 text-center"><logo></logo></div>
                 <paquete></paquete>
             </div>
+            <div class="row">
+                <gomitas></gomitas>
+            </div>
         </main>
         
     
 </template>
 <script>
+import Gomitas from './components/Gomitas.vue';
 import Logo from './components/Logo.vue';
 import Paquete from './components/Paquete.vue';
 export default {
-    components: { Logo, Paquete },
+    components: { Logo, Paquete,
+        Gomitas },
 }
 </script>
 
