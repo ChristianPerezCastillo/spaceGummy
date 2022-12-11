48b3a3ade10500775be9718bad90c355286d7770
ChristianPerezCastillo <ch.hristian96@gmail.com>
2022-12-11 17:19:40 -0600

    Prueba de js

--- src/components/Gomitas.vue
+++ src/components/Gomitas.vue
@@ -4,7 +4,7 @@
         <h2 class="neon fs-2">SELECCIONA TUS PRODUCTOS</h2>
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
                           <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="picafresaSelect">
-                              <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
+                              <img src="../assets/img/picafresas.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">PICAFRESA</p>
                           </figure>
         </div>
@@ -46,25 +46,25 @@
         </div>
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
                           <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="naturalSelect">
-                              <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
+                              <img src="../assets/img/chips.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">PAPAS NATURALES</p>
                           </figure>
         </div>
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
                           <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="adobadoSelect">
-                              <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
+                              <img src="../assets/img/adobadas.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">PAPAS ADOBADAS</p>
                           </figure>
         </div>
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
                           <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="hotnutSelect">
-                              <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
+                              <img src="../assets/img/hotnuts.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">HOT NUTS</p>
                           </figure>
         </div>
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
                           <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="japonesSelect">
-                              <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
+                              <img src="../assets/img/japoneses.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">JAPONESES</p>
                           </figure>
         </div>
@@ -73,43 +73,43 @@
         <h2 class="neon fs-2">SELECCIONA TUS SALSAS</h2>
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
                           <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="chamoySelect">
-                              <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
+                              <img src="../assets/img/chamoy.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">CHAMOY</p>
                           </figure>
         </div>
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
                           <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="botaneraSelect">
-                              <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
+                              <img src="../assets/img/botanera.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">BOTANERA</p>
                           </figure>
         </div>
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
                           <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="maggySelect">
-                              <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
+                              <img src="../assets/img/maggy.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">MAGGY</p>
                           </figure>
         </div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
+        <!-- <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
                           <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="inglesaSelect">
-                              <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
+                              <img src="../assets/img/inglesa.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">SALSA INGLESA</p>
                           </figure>
-        </div>
+        </div> -->
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
                           <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="limonSelect">
-                              <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
+                              <img src="../assets/img/limon.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">LIMON</p>
                           </figure>
         </div>
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
                           <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="tajinSelect">
-                              <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
+                              <img src="../assets/img/tajin.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">TAJIN</p>
                           </figure>
         </div>
         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
                           <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="miguelitoSelect">
-                              <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
+                              <img src="../assets/img/miguelito.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">MIGUELITO</p>
                           </figure>
         </div>
