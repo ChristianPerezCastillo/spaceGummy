84d169ca427ad83a97a0225ec47e8755be01730a
ChristianPerezCastillo <ch.hristian96@gmail.com>
2022-12-10 18:18:47 -0600

    Modificacion de estilos

--- src/components/Gomitas.vue
+++ src/components/Gomitas.vue
@@ -3,25 +3,37 @@
       <div class="row m-4">
         <div class="col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
                           <div class="bg-transparent glass">
-                              <img src="../assets/img/gummy.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
+                              <img src="../assets/img/aciditos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
+                              <p class="neon">ACIDITOS</p>
+                          </div>
+        </div>
+        <div class="col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
+                          <div class="bg-transparent glass">
+                              <img src="../assets/img/aritos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p>GOMITAS</p>
                           </div>
         </div>
         <div class="col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
                           <div class="bg-transparent glass">
-                              <img src="../assets/img/gummy.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
+                              <img src="../assets/img/aciditos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p>GOMITAS</p>
                           </div>
         </div>
         <div class="col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
                           <div class="bg-transparent glass">
-                              <img src="../assets/img/gummy.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
+                              <img src="../assets/img/aritos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p>GOMITAS</p>
                           </div>
         </div>
         <div class="col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
                           <div class="bg-transparent glass">
-                              <img src="../assets/img/gummy.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
+                              <img src="../assets/img/dulcigomas.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
+                              <p>GOMITAS</p>
+                          </div>
+        </div>
+        <div class="col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
+                          <div class="bg-transparent glass">
+                              <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p>GOMITAS</p>
                           </div>
         </div>
--- src/components/Paquete.vue
+++ src/components/Paquete.vue
@@ -1,10 +1,13 @@
 <template>  
-<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-6 text-center my-auto p-4">
-                <article id="paqueteGrande"><button class="button-67" role="button" id="btnGde">Paquete grande</button></article>
-                
-                <article id="paqueteChico"><button class="button-67" role="button" id="btnChico">Paquete chico</button></article>
-                
-    
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-6 text-center my-auto">
+                <div class="row">
+                    <div class="col-md-6 mt-4 d-flex justify-content-center align-items-center" id="paqueteGrande">
+                        <img src="../assets/img/gummy.png" class="card-img-top" alt="..." style="width: 12rem; height: 12   rem;" id="btnGde">
+                    </div>
+                    <div class="col-md-6 mt-4 d-flex justify-content-center align-items-center" id="paqueteChico">
+                        <img src="../assets/img/gummy.png" class="card-img-top d-flex justify-content-center align-items-center" alt="..." style="width: 15rem; height: 15rem;" id="btnGde">
+                    </div>
+                </div>
              
            </div>
         
