b2cc0142158e7287b048071edfed371b1f06e60b
ChristianPerezCastillo <ch.hristian96@gmail.com>
2022-12-11 14:13:12 -0600

    Se modificaron estilos y se agrego js

--- src/components/Gomitas.vue
+++ src/components/Gomitas.vue
@@ -1,112 +1,112 @@
 <template>
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 text-center my-auto p-4 mt-4  swiper mySwiper">
-      <div class="row m-4 menu--wrapper">
-        <div class="menu--item col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
-                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 text-center my-auto p-4 mt-4">
+      <div class="row m-4">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
+                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="aciditoSelect">
                               <img src="../assets/img/aciditos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">ACIDITOS</p>
                           </figure>
         </div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
-                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
+                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="aritoSelect">
                               <img src="../assets/img/aritos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">ARITOS</p>
                           </figure>
         </div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
-                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
+                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="manguitoSelect">
                               <img src="../assets/img/manguitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">MANGUITOS</p>
                           </figure>
         </div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
-                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
+                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="panditaSelect">
                               <img src="../assets/img/panditas.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">PANDITAS</p>
                           </figure>
         </div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
-                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
+                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="dulcigomaSelect">
                               <img src="../assets/img/dulcigomas.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">DULCIGOMAS</p>
                           </figure>
         </div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
-                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
+                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="gusanitoSelect">
                               <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">GUSANITOS</p>
                           </figure>
         </div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
-                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
+                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="naturalSelect">
                               <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">PAPAS NATURALES</p>
                           </figure>
         </div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
-                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
+                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="adobadoSelect">
                               <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">PAPAS ADOBADAS</p>
                           </figure>
         </div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
-                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
+                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="hotnutSelect">
                               <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">HOT NUTS</p>
                           </figure>
         </div>
-        <div class="col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
-                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;">
+        <div class="col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
+                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="japonesSelect">
                               <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">JAPONESES</p>
                           </figure>
         </div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
-                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
+                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="chamoySelect">
                               <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">CHAMOY</p>
                           </figure>
         </div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
-                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
+                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="botaneraSelect">
                               <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">BOTANERA</p>
                           </figure>
         </div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
-                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
+                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="maggySelect">
                               <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">MAGGY</p>
                           </figure>
         </div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
-                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
+                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="inglesaSelect">
                               <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">SALSA INGLESA</p>
                           </figure>
         </div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
-                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
+                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="limonSelect">
                               <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">LIMON</p>
                           </figure>
         </div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
-                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
+                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="tajinSelect">
                               <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">TAJIN</p>
                           </figure>
         </div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
-                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
+                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="miguelitoSelect">
                               <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
                               <p class="neon">MIGUELITO</p>
                           </figure>
         </div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center" id="panditaSelect">
-                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 p-4 d-flex justify-content-center align-items-center">
+                          <figure class="bg-transparent glass" style="width: 15rem; height: 15rem;" id="picafresaSelect">
                               <img src="../assets/img/gusanitos.png" class="card-img-top" alt="..." style="width: 10rem; height: 10rem;">
-                              <p class="neon">MIGUELITO</p>
+                              <p class="neon">PICAFRESA</p>
                           </figure>
         </div>
         
--- src/main.js
+++ src/main.js
@@ -11,8 +11,79 @@ const paqueteGrande = document.getElementById('paqueteGrande');
 const paqueteChico = document.getElementById('paqueteChico');
 const descripcionPaqueteGrande = document.getElementById('descripcionPaqueteGrande');
 const descripcionPaqueteChico = document.getElementById('descripcionPaqueteChico');
-
+const aciditoSelect = document.getElementById('aciditoSelect');
+const aritoSelect = document.getElementById('aritoSelect');
+const manguitoSelect = document.getElementById('manguitoSelect');
+const panditaSelect = document.getElementById('panditaSelect');
+const dulcigomaSelect = document.getElementById('dulcigomaSelect');
+const gusanitoSelect = document.getElementById('gusanitoSelect');
+const naturalSelect = document.getElementById('naturalSelect');
+const adobadoSelect = document.getElementById('adobadoSelect');
+const hotnutSelect = document.getElementById('hotnutSelect');
+const japonesSelect = document.getElementById('japonesSelect');
+const chamoySelect = document.getElementById('chamoySelect');
+const botaneraSelect = document.getElementById('botaneraSelect');
+const maggySelect = document.getElementById('maggySelect');
+const inglesaSelect = document.getElementById('inglesaSelect');
+const limonSelect = document.getElementById('limonSelect');
+const tajinSelect = document.getElementById('tajinSelect');
+const miguelitoSelect = document.getElementById('miguelitoSelect');
+const picafresaSelect = document.getElementById('picafresaSelect');
+var tickets = null;
 // Asignamos una función al evento click del botón
+aciditoSelect.addEventListener('click', function() {
+  selection(aciditoSelect)});
+// Asignamos una función al evento click del botón
+aritoSelect.addEventListener('click', function() {
+  selection(aritoSelect)});
+// Asignamos una función al evento click del botón
+manguitoSelect.addEventListener('click', function() {
+  selection(manguitoSelect)});
+// Asignamos una función al evento click del botón
+panditaSelect.addEventListener('click', function() {
+  selection(panditaSelect)});
+// Asignamos una función al evento click del botón
+dulcigomaSelect.addEventListener('click', function() {
+  selection(dulcigomaSelect)});
+// Asignamos una función al evento click del botón
+gusanitoSelect.addEventListener('click', function() {
+  selection(gusanitoSelect)});
+// Asignamos una función al evento click del botón
+naturalSelect.addEventListener('click', function() {
+  selection(naturalSelect)});
+// Asignamos una función al evento click del botón
+adobadoSelect.addEventListener('click', function() {
+  selection(adobadoSelect)});
+// Asignamos una función al evento click del botón
+hotnutSelect.addEventListener('click', function() {
+  selection(hotnutSelect)});
+// Asignamos una función al evento click del botón
+japonesSelect.addEventListener('click', function() {
+  selection(japonesSelect)});
+// Asignamos una función al evento click del botón
+chamoySelect.addEventListener('click', function() {
+  selection(chamoySelect)});
+// Asignamos una función al evento click del botón
+botaneraSelect.addEventListener('click', function() {
+  selection(botaneraSelect)});
+// Asignamos una función al evento click del botón
+maggySelect.addEventListener('click', function() {
+  selection(maggySelect)});
+// Asignamos una función al evento click del botón
+inglesaSelect.addEventListener('click', function() {
+  selection(inglesaSelect)});
+// Asignamos una función al evento click del botón
+limonSelect.addEventListener('click', function() {
+  selection(limonSelect)});
+// Asignamos una función al evento click del botón
+tajinSelect.addEventListener('click', function() {
+  selection(tajinSelect)});
+// Asignamos una función al evento click del botón
+miguelitoSelect.addEventListener('click', function() {
+  selection(miguelitoSelect)});
+// Asignamos una función al evento click del botón
+picafresaSelect.addEventListener('click', function() {
+  selection(picafresaSelect)});
 paqueteGrande.addEventListener('click', function() {
   // Ocultamos el elemento <div> cuando se presiona el botón
   if(paqueteChico.classList.contains('d-none')){
@@ -33,6 +104,7 @@ paqueteGrande.addEventListener('click', function() {
     paqueteChico.classList.add('col-md-6');
     paqueteGrande.classList.remove('col-md-12');
     paqueteGrande.classList.add('col-md-6');
+    tickets = null;
   }else{
 
     paqueteChico.classList.remove('col-xl-6');
@@ -52,6 +124,21 @@ paqueteGrande.addEventListener('click', function() {
 
     paqueteChico.classList.add('d-none');
     descripcionPaqueteGrande.classList.remove('d-none');
+    tickets = 6;
+    
+    // Asignamos una función al evento click del botón
+
+  reload(aciditoSelect);
+  reload(aritoSelect);
+  reload(manguitoSelect);
+  reload(panditaSelect);
+  reload(dulcigomaSelect);
+  reload(gusanitoSelect);
+  reload(naturalSelect);
+  reload(adobadoSelect);
+  reload(hotnutSelect);
+  reload(japonesSelect);
+  reload(picafresaSelect);
   }
 });
 paqueteChico.addEventListener('click', function() {
@@ -75,6 +162,8 @@ paqueteChico.addEventListener('click', function() {
     paqueteChico.classList.add('col-md-6');
     paqueteGrande.classList.remove('col-md-12');
     paqueteGrande.classList.add('col-md-6');
+    tickets = null;
+    
   }else{
 
     paqueteChico.classList.remove('col-xl-6');
@@ -94,6 +183,7 @@ paqueteChico.addEventListener('click', function() {
     paqueteGrande.classList.add('col-md-12');
     
     descripcionPaqueteChico.classList.remove('d-none');
+    tickets = 11;
   }
 });
 
@@ -111,3 +201,60 @@ const typed = new Typed('.typed', {
     cursorChar: '|',
 });
 
+function selection(select){
+  // Ocultamos el elemento <div> cuando se presiona el botón
+  if(tickets != null){
+    
+      if(select.classList.contains('glass')){
+        select.classList.remove('glass');
+        select.classList.add('glassSelect');
+        tickets--;
+  
+      }else{
+        select.classList.add('glass');
+        select.classList.remove('glassSelect');
+        tickets++;
+      }
+      if(tickets == 0){
+      aciditoSelect.classList.contains('glassSelect')? null : bloquear(aciditoSelect);
+      aritoSelect.classList.contains('glassSelect')? null : bloquear(aritoSelect);
+      manguitoSelect.classList.contains('glassSelect')? null : bloquear(manguitoSelect);
+      panditaSelect.classList.contains('glassSelect')? null : bloquear(panditaSelect);
+      dulcigomaSelect.classList.contains('glassSelect')? null : bloquear(dulcigomaSelect);
+      gusanitoSelect.classList.contains('glassSelect')? null : bloquear(gusanitoSelect);
+      naturalSelect.classList.contains('glassSelect')? null : bloquear(naturalSelect);
+      adobadoSelect.classList.contains('glassSelect')? null : bloquear(adobadoSelect);
+      hotnutSelect.classList.contains('glassSelect')? null : bloquear(hotnutSelect);
+      japonesSelect.classList.contains('glassSelect')? null : bloquear(japonesSelect);
+      picafresaSelect.classList.contains('glassSelect')? null : bloquear(picafresaSelect);
+      }else{
+        aciditoSelect.classList.contains('glassSelect')? null : desbloquear(aciditoSelect);
+      aritoSelect.classList.contains('glassSelect')? null : desbloquear(aritoSelect);
+      manguitoSelect.classList.contains('glassSelect')? null : desbloquear(manguitoSelect);
+      panditaSelect.classList.contains('glassSelect')? null : desbloquear(panditaSelect);
+      dulcigomaSelect.classList.contains('glassSelect')? null : desbloquear(dulcigomaSelect);
+      gusanitoSelect.classList.contains('glassSelect')? null : desbloquear(gusanitoSelect);
+      naturalSelect.classList.contains('glassSelect')? null : desbloquear(naturalSelect);
+      adobadoSelect.classList.contains('glassSelect')? null : desbloquear(adobadoSelect);
+      hotnutSelect.classList.contains('glassSelect')? null : desbloquear(hotnutSelect);
+      japonesSelect.classList.contains('glassSelect')? null : desbloquear(japonesSelect);
+      picafresaSelect.classList.contains('glassSelect')? null : desbloquear(picafresaSelect);
+      
+      }
+    
+  }else{
+    $('#miModal').modal('show');
+  }
+}
+
+function reload(select){
+  select.classList.add('glass');
+  select.classList.remove('glassSelect');
+}
+
+function bloquear(select){
+  select.classList.add('disabled');
+}
+function desbloquear(select){
+  select.classList.remove('disabled');
+}
