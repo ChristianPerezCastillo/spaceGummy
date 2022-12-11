802881854d52d2313ed04fb8126b1524be85a8ea
ChristianPerezCastillo <ch.hristian96@gmail.com>
2022-12-10 22:32:06 -0600

    Modificacion de estilos

--- src/components/Paquete.vue
+++ src/components/Paquete.vue
@@ -1,10 +1,10 @@
 <template>  
 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-6 text-center my-auto">
                 <div class="row">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 col-xxl-6 d-flex justify-content-center align-items-center" id="paqueteGrande">
+                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6 d-flex justify-content-center align-items-center" id="paqueteGrande">
                         <img src="../assets/img/gummy.png" class="card-img-top" alt="..." style="width: 12rem; height: 12   rem;" id="btnGde">
                     </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 col-xxl-6 d-flex justify-content-center align-items-center" id="paqueteChico">
+                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6 d-flex justify-content-center align-items-center" id="paqueteChico">
                         <img src="../assets/img/gummy.png" class="card-img-top d-flex justify-content-center align-items-center" alt="..." style="width: 15rem; height: 15rem;" id="btnGde">
                     </div>
                 </div>
